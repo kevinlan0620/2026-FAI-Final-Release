81492e95a55691411259ccb9a5002d103384e724
chun0620 <kevinlan.gemini@gmail.com>
2026-04-16 23:00:38 +0800

    0416 test

--- src/players/b12902001/best_player1.py
+++ src/players/b12902001/best_player1.py
@@ -3,5 +3,17 @@ class best_player1:
         self.player_idx = player_idx
     
     def action(self, hand, history):
-
-        return hand[len(hand)-1]
+        threshold_min = 20
+        min_diff = 104
+        for i in range(len(hand)-1):
+            if hand[i] < threshold_min:
+                return hand[i]
+            else:
+                for j in range(4):
+                    if hand[i] > history.board[j][len(history.board[j])-1]:
+                        diff = hand[i] - history.board[j][len(history.board[j])-1]
+                        if diff < min_diff and len(history.board[j]) < 5:
+                            min_diff = i
+        if min_diff == 104:
+            return hand[len(hand)-1]
+        return hand[min_diff]
